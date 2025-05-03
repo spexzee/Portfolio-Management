ea9f11617ca5fb8e8377bd40c27bf15c627b110b
Abu Bakar (Abu) <abub5979@gmail.com>
2025-05-03 08:37:47 +0000

    hydration err

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={cn(
           geistSans.variable,
